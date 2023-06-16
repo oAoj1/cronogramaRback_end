f4929bd87be44122437ed3d5f66dd45866c610d9
oAoj1 <joaogbmarques@gmail.com>
2023-06-16 11:11:50 -0300

    Update mongoConnect.js

--- src/config/mongoConnect.js
+++ src/config/mongoConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_CONNECT)
+mongoose.connect('mongodb+srv://joao:123@cluster0.6nuspyb.mongodb.net/dias-semana')
 
 let db = mongoose.connection
 
