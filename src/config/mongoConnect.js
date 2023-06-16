afd0d381f45971b8e70ce99a25e607c10657520d
oAoj1 <joaogbmarques@gmail.com>
2023-06-16 11:03:39 -0300

    Update mongoConnect.js

--- src/config/mongoConnect.js
+++ src/config/mongoConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const uri = process.env.MONGO_CONNECT
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.createConnect(uri)
 
 let db = mongoose.connection
 
