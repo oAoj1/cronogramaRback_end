c69218a319c0d82672b36c5ab3a7fcfda561d3dc
oAoj1 <joaogbmarques@gmail.com>
2023-06-22 22:53:38 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -44,67 +44,6 @@ app.listen(port, () => console.log(`Server ligado => http://localhost:${port}`))
 
 var data = new Date()
 
-app.get('/tempo/dia', (req,res) => {
-    let diaHoje = data.getDay()
-
-    if(diaHoje == 0){
-        diaHoje = 'Domingo'
-    }
-    if(diaHoje == 1){
-        diaHoje = 'Segunda-feira'
-    }
-    if(diaHoje == 2){
-        diaHoje = 'Terça-feira'
-    }
-    if(diaHoje == 3){
-        diaHoje = 'Quarta-feira'
-    }
-    if(diaHoje == 4){
-        diaHoje = 'Quinta-feira'
-    }
-    if(diaHoje == 5){
-        diaHoje = 'Sexta-feira'
-    }
-    if(diaHoje == 6){
-        diaHoje = 'Sábado'
-    }
-
-    res.send(diaHoje)
-
-})
-
-app.get('/tempo/data', (req,res) => {
-    let dia = String(data.getDate()).padStart(2,'0')
-    let mes = String(data.getMonth() + 1).padStart(2,'0')
-
-    let dataCompleta = `${dia}/${mes}`
-
-    res.send(dataCompleta)
-
-})
-
-app.get('/tempo/periodoagora', (req,res) => {
-    let hora = data.getHours()
-    var periodoAgora = ''
-
-    if(hora >= 0 && hora <= 5){
-        periodoAgora = 'Madrugada'
-        
-    }else if(hora >= 6 && hora <= 11){
-        periodoAgora = 'Manhã'
-        
-    }else if(hora >= 12 && hora <= 17){
-        periodoAgora = 'Tarde'
-
-    }else if(hora >= 18 && hora <= 23){
-        periodoAgora = 'Noite'
-
-    }
-
-    res.send(periodoAgora)
-    
-})
-
 app.get('/senha', (req,res) => {
     const senhaSistema = process.env.PASSWORD
     console.log(senhaSistema)
